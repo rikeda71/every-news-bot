a5df765ce9d7f3bd044d8116c03604da1d9bb159
Ryuya Ikeda <rikeda71@gmail.com>
2020-08-22 18:17:59 +0900

    Add Line Bot sample codes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/s14t284/news-bot-lambda
 
 go 1.15
+
+require github.com/line/line-bot-sdk-go v7.5.0+incompatible // indirect
--- main.go
+++ main.go
@@ -2,18 +2,68 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/line/line-bot-sdk-go/linebot/httphandler"
 	"github.com/s14t284/news-bot-lambda/service"
 )
 
 // 主要カテゴリのニュース
 const (
-	RssURL = "https://news.yahoo.co.jp/pickup/rss.xml"
+	Main          = "https://news.yahoo.co.jp/pickup/rss.xml"               // 主要
+	Domestic      = "https://news.yahoo.co.jp/pickup/domestic/rss.xml"      // 国内
+	World         = "https://news.yahoo.co.jp/pickup/world/rss.xml"         // 国際
+	Economy       = "https//news.yahoo.co.jp/pickup/economy/rss.xml"        // 経済
+	Entertainment = "https://news.yahoo.co.jp/pickup/entertainment/rss.xml" // エンタメ
+	Sports        = "https://news.yahoo.co.jp/pickup/sports/rss.xml"        // スポーツ
+	Computer      = "https://news.yahoo.co.jp/pickup/computer/rss.xml"      // IT
+	Science       = "https://news.yahoo.co.jp/pickup/science/rss.xml"       // 科学
+	Local         = "https://news.yahoo.co.jp/pickup/local/rss.xml"         // 地域
 )
+
+func getLineHandler() (*httphandler.WebhookHandler, error) {
+	handler, err := httphandler.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Print(err.Error())
+		return nil, err
+	}
+	handler.HandleEvents(func(events []*linebot.Event, r *http.Request) {
+		bot, err := handler.NewClient()
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+	return handler, nil
+}
 
 func main() {
 	var client service.RssReader = &service.RssClient{}
-	if resp, err := client.Request(RssURL); err == nil {
-		fmt.Println(resp)
+	handler, err := getLineHandler()
+	if err != nil {
+		return
+	}
+	http.Handle("/callback", handler)
+	if resp, err := client.Request(Main); err == nil {
+		for _, news := range resp.Items {
+			fmt.Println(news.Title)
+			fmt.Println(news.Link)
+		}
 	}
 }
